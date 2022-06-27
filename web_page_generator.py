9316ca84e190211e28e2467e6e6cb1295d7a606f
renatog33 <101902010+renatog33@users.noreply.github.com>
2022-06-27 10:32:50 -0700

    Update web_page_generator.py

--- web_page_generator.py
+++ web_page_generator.py
@@ -4,7 +4,7 @@ from tkinter import *
 win = Tk()
 l = Label(win, text="Enter the body text for the page and click Submit")
 l.pack(side= TOP, padx= 10, pady=5)
-b1 = Button(win, text="Submit")
+b1 = Button(win, text="Submit", command = lambda:newtext())
 b1.pack(side=RIGHT, padx =10)
 
 v = StringVar()
@@ -14,16 +14,19 @@ e.pack(side=LEFT, padx= 10)
 v.get()
 
 import webbrowser
-f = open("summer_sale.html", "w")
+def newtext():
+    
+    f = open("summer_sale.html", "w")
+    
 
-message = ("""<html><body>
-<h1>
-Stay tuned for our amazing summer sale!
-</h1>
-</body>
-</html>""")
-f.write(message)
-f.close()
+    message = ("""<html><body>
+    <h1>
+    {}
+    </h1>
+    </body>
+    </html>""".format(v.get()))
+    f.write(message)
+    f.close()
 
-webbrowser.open_new_tab('summer_sale.html')
+    webbrowser.open_new_tab('summer_sale.html')
 
